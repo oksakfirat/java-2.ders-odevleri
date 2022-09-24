71a318bd1a5f7c153a62ce3bddae775daaeb7f82
Genel Kullanicilar <firatoksak23@gmail.com>
2022-09-24 21:45:13 +0300

    java

--- reCapDemo_Classes/src/reCapDemo_Classes/Main.java
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		DortIslem dortIslem = new DortIslem();
-		int toplam = dortIslem.Topla(23, 12);
-		int cikarma = dortIslem.Cikar(23, 12);
-		int carpma = dortIslem.Carp(12, 23);
-		double bolme = dortIslem.Bolme(23, 12);
+		int toplam = dortIslem.topla(23, 12);
+		int cikarma = dortIslem.cikar(23, 12);
+		int carpma = dortIslem.carp(12, 23);
+		double bolme = dortIslem.bolme(23, 12);
 
 		System.out.println(toplam);
 		System.out.println(cikarma);
