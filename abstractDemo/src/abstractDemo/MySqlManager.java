9016d55e67596cf0980f1f0a3e196d28ef800d5a
Genel Kullanicilar <firatoksak23@gmail.com>
2022-10-09 20:55:55 +0300

    java

---
+++ abstractDemo/src/abstractDemo/MySqlManager.java
@@ -0,0 +1,11 @@
+package abstractDemo;
+
+public class MySqlManager  extends BaseDatabaseManager{
+
+	@Override
+	public void getData() {
+		System.out.println("Veri getirildi: MySql");
+		
+	}
+
+}
